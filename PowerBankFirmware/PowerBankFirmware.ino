269acd2bc081db732188a89cdb9df66fe8d58337
Pavel Palonen <pavel.palonen@gmail.com>
2019-10-25 13:00:44 +0200

    working ADC delay

--- PowerBankFirmware/PowerBankFirmware.ino
+++ PowerBankFirmware/PowerBankFirmware.ino
@@ -9,22 +9,25 @@
 // ADC = Vin * 1024 / Vref
 
 uint16_t delayReadADC(){
-	//int i = 0;
+	//uint16_t c = 0;
 	while (bit_is_set(ADCSRA,ADSC));
-	for(uint16_t i=0,i<60000,i++){ // about 1 sec
+	for(uint16_t i=0;i<2000;i++){ // about 1 sec
+    //sleep_enable();
+    //sleep_cpu();
+
 		bitSet (ADCSRA, ADSC); // Start conversion
 		while (bit_is_set(ADCSRA,ADSC));
 		//return ADC; // read the value
 		uint8_t low  = ADCL; // must read ADCL first
 		uint8_t high = ADCH;
-		return (uint16_t)((high<<8) | low);
-
+    //c=(uint16_t)((high<<8) | low);
 	}
+  //return c;
 }
 
 void setup() {
-  cli; // no interrupts
-  //noInterrupts();
+  //cli; // no interrupts
+  noInterrupts();
 
   // power reduction
   PRR = (1<<PRTIM1) | (1<<PRTIM0) | (1<<PRUSI);
@@ -38,7 +41,7 @@ void setup() {
   // initialize ADC
   DIDR0 = (1<<ADC1D); // disable digital input logic on ADC1
   ADMUX = (1<<MUX0);	// ADC1 on PB2
-  ADCSRA = (1<<ADEN) | (1<<ADPS2); // enable ADC with 16 divider and start first convertion
+  ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1)| (1<<ADPS0); // enable ADC with 16 divider and start first convertion
   //ADCSRA |= (1<<ADSC); // Start conversion
   bitSet (ADCSRA, ADSC); // Start conversion
   //while (ADCSRA & (1 << ADSC)); // wait for a/d complete
@@ -50,6 +53,7 @@ void setup() {
 }
 
 void loop() {
+  uint16_t tmp;
   bitSet(DDRB, LED_RED);
   delayReadADC();
   bitClear(DDRB, LED_RED);
