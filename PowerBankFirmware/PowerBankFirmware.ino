b8c21b870d752920d92e2fff197eca8c0e2f1352
Pavel Palonen <pavel.palonen@gmail.com>
2019-10-25 23:16:00 +0200

    Final working version

--- PowerBankFirmware/PowerBankFirmware.ino
+++ PowerBankFirmware/PowerBankFirmware.ino
@@ -16,17 +16,17 @@
 // 4.1V = 999 ADC    |             |
 // 4.0V = 975 ADC    |             | YELLOW
 // 3.8V = 926 ADC    |             |
-// 3.7V = 902 ADC    | YELLOW      |
-// 3.6V = 877 ADC    |             |
+// 3.7V = 902 ADC    |             |
+// 3.6V = 877 ADC    | YELLOW      |
 // 3.5V = 853 ADC    |             | RED
 // 3.4V = 828 ADC    |             |
 // 3.3V = 804 ADC    |             |
 // 3.2V = 780 ADC    |             |
-// 3.1V = 755 ADC    |             |
-// 3.0V = 731 ADC    | RED         | RED BLINK
+// 3.1V = 755 ADC    | RED         |
+// 3.0V = 731 ADC    |             | RED BLINK
 // 2.8V = 682 ADC    | RED BLINK   | 
 
-uint8_t charging = 0; // 0 - not charging; 1 - charging
+bool charging = LOW; // 0 - not charging; 1 - charging
 uint8_t ledmode = 0; // 0 - RED BLINK; 1 - RED; 2 - YELLOW; 3 - GREEN
 int oldADCval=0;
 uint8_t blink=0;
@@ -38,10 +38,10 @@ void setLED(int curADC){
 	if(hystval<0) hystval=-hystval;
 	if(hystval>hyst){
 		oldADCval = curADC;
-		if(charging==0){
+		if(!charging){
 			// not charging pattern
-			if(curADC>902) newledmode=3;
-			else if(curADC>731) newledmode=2;
+			if(curADC>877) newledmode=3;
+			else if(curADC>755) newledmode=2;
 			else if(curADC>682) newledmode=1;
 			else newledmode=0;
 		} else {
@@ -53,7 +53,6 @@ void setLED(int curADC){
 		}
 		// set the LED
 		if(newledmode!=ledmode){
-			//DDRB = 0; // turn off all leds
 			ledmode = newledmode;
 			if(ledmode==3) DDRB = 1<<LED_GREEN;
 			else if(ledmode==2) DDRB = 1<<LED_ORANGE;
@@ -66,13 +65,12 @@ void setLED(int curADC){
 uint16_t ReadADC(){
 	uint16_t res32=0;	// accumulator register for ADC readings
 	while (bit_is_set(ADCSRA,ADSC)); // wait for any unclompleted ADC conversions
-	for(uint16_t i=0;i<5300;i++){ // about 1 sec. In reality will be 32 cycles
-		do{
+	for(uint8_t i=0;i<32;i++){ // about 1 sec. In reality will be 32 cycles
+		//do{
 			sei(); // conversion should start automatically after entering sleep mode
 			sleep_cpu();
 			cli();
-			//bitSet (ADCSRA, ADSC); // Start conversion
-		} while (bit_is_set(ADCSRA,ADSC));
+		//} while (bit_is_set(ADCSRA,ADSC));
 		//return ADC; // read the value
 		uint8_t low  = ADCL; // must read ADCL first
 		uint8_t high = ADCH;
@@ -89,10 +87,9 @@ void setup() {
   TIMSK = 0; // disable timers interrupts
   TCCR1 = (1<<CS13) | (1<<CS12); // bur tick timer with 2048 prescaller
   GIMSK = 0; // disable pin interrupts
-  //noInterrupts();
 
   // power reduction
-  PRR = (1<<PRTIM1) | (1<<PRTIM0) | (1<<PRUSI);
+  PRR = (1<<PRTIM0) | (1<<PRUSI);
   ACSR = (1<<ACD); // power down analog comparator
 
   //initialize pins
@@ -101,20 +98,20 @@ void setup() {
   PORTB = 0;
   
   // initialize ADC
-  DIDR0 = (1<<ADC1D); // disable digital input logic on ADC1
+  //DIDR0 = (1<<ADC1D); // disable digital input logic on ADC1
   ADMUX = (1<<MUX0);	// ADC1 on PB2
   ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADIE); // enable ADC with 16 divider and start first convertion. Interrupt should be enabled to wake up after conversion is completed
-  //ADCSRA |= (1<<ADSC); // Start conversion
   bitSet (ADCSRA, ADSC); // Start conversion
-  //while (ADCSRA & (1 << ADSC)); // wait for a/d complete
   while (bit_is_set(ADCSRA,ADSC)); // wait for a/d complete
   
   //set_sleep_mode(SLEEP_MODE_ADC);
   set_sleep_mode(SLEEP_MODE_IDLE); // we want timers to run
   sleep_enable(); // set sleep enable bit
+
 }
 
 void loop() {
+  
 	uint8_t tmptmr = TCNT1;
 	if(ledmode==0){ // only blink in mode 0
 		if(tmptmr>128) DDRB = 1<<LED_RED; else DDRB = 0;
@@ -123,7 +120,11 @@ void loop() {
 	uint16_t adcVal = ReadADC();
 	setLED(adcVal);
 	
-	// TODO check charge sensing pin (after schematic is ready)
+	if(charging != bitRead(PINB,CHARGESENSEPIN)){
+    charging = !charging;
+    oldADCval = 0; //Force led update
+	}
+ 
 /*
   bitSet(DDRB, LED_RED);
   ReadADC();
@@ -132,7 +133,7 @@ void loop() {
   ReadADC();
   bitClear(DDRB, LED_ORANGE);
   bitSet(DDRB, LED_GREEN);
-  ReadADC());
+  ReadADC();
   bitClear(DDRB, LED_GREEN);
 */
 }
