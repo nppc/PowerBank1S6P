216941ce1bcc046fb86dac993408b7ba798fb4a9
Palonen <ppalonen@bethel.jw.org>
2019-10-25 11:59:01 +0200

    ADC timing test

--- PowerBankFirmware/PowerBankFirmware.ino
+++ PowerBankFirmware/PowerBankFirmware.ino
@@ -1,24 +1,63 @@
-// PB2 - ADC inout from battery
-// PB1 - Red LED
-// PB3 - Orange LED
-// PB4 - Green LED
+#include <avr/sleep.h>
+
+#define LED_RED PB1
+#define LED_ORANGE PB3
+#define LED_GREEN PB4
+#define ADCPIN PB2 //ADC1
+#define CHARGESENSEPIN PB0
+
+// ADC = Vin * 1024 / Vref
+
+uint16_t delayReadADC(){
+	//int i = 0;
+	while (bit_is_set(ADCSRA,ADSC));
+	for(uint16_t i=0,i<60000,i++){ // about 1 sec
+		bitSet (ADCSRA, ADSC); // Start conversion
+		while (bit_is_set(ADCSRA,ADSC));
+		//return ADC; // read the value
+		uint8_t low  = ADCL; // must read ADCL first
+		uint8_t high = ADCH;
+		return (uint16_t)((high<<8) | low);
+
+	}
+}
+
 void setup() {
-  digitalWrite(PB1, LOW);
-  pinMode(PB1, INPUT);
-  digitalWrite(PB3, LOW);
-  pinMode(PB3, INPUT);
-  digitalWrite(PB4, LOW);
-  pinMode(PB4, INPUT);
+  cli; // no interrupts
+  //noInterrupts();
+
+  // power reduction
+  PRR = (1<<PRTIM1) | (1<<PRTIM0) | (1<<PRUSI);
+  ACSR = (1<<ACD); // power down analog comparator
+
+  //initialize pins
+  //Float LED pins (OFF)
+  DDRB = 0;
+  PORTB = 0;
+  
+  // initialize ADC
+  DIDR0 = (1<<ADC1D); // disable digital input logic on ADC1
+  ADMUX = (1<<MUX0);	// ADC1 on PB2
+  ADCSRA = (1<<ADEN) | (1<<ADPS2); // enable ADC with 16 divider and start first convertion
+  //ADCSRA |= (1<<ADSC); // Start conversion
+  bitSet (ADCSRA, ADSC); // Start conversion
+  //while (ADCSRA & (1 << ADSC)); // wait for a/d complete
+  while (bit_is_set(ADCSRA,ADSC)); // wait for a/d complete
+  
+  set_sleep_mode(SLEEP_MODE_ADC);
+  //sleep_enable();
+  //sleep_cpu();
 }
 
 void loop() {
-  pinMode(PB1, OUTPUT);   
-  delay(2000);            
-  pinMode(PB1, INPUT);    
-  pinMode(PB3, OUTPUT);   
-  delay(2000);            
-  pinMode(PB3, INPUT);    
-  pinMode(PB4, OUTPUT);   
-  delay(2000);             
-  pinMode(PB4, INPUT);     
+  bitSet(DDRB, LED_RED);
+  delayReadADC();
+  bitClear(DDRB, LED_RED);
+  bitSet(DDRB, LED_ORANGE);
+  delayReadADC();
+  bitClear(DDRB, LED_ORANGE);
+  bitSet(DDRB, LED_GREEN);
+  delayReadADC();
+  bitClear(DDRB, LED_GREEN);
 }
+
